1d48bb3d1b42e7eab46b6e1d45190032c4ddf003
samsamsam <samsamsam@o2.pl>
2024-12-28 08:53:46 +0100

    py3 remove dependency to pkg_resources/pkgutil from google/

--- protobuf-3.17.3/google_py3/__init__.py
+++ protobuf-3.17.3/google_py3/__init__.py
@@ -1,4 +1,7 @@
 try:
   __import__('pkg_resources').declare_namespace(__name__)
-except Exception:
-  __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+except ImportError:
+  try:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+  except ImportError:
+    pass
