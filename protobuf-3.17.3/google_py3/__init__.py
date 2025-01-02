5fc6e899200dbbfbbef695053453620a7fa6bdc2
samsamsam <samsamsam@o2.pl>
2025-01-02 14:02:05 +0100

    Ignore all errors in pkg_resources/pkg util import not only import exception

--- protobuf-3.17.3/google_py3/__init__.py
+++ protobuf-3.17.3/google_py3/__init__.py
@@ -1,7 +1,7 @@
 try:
   __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
+except Exception:
   try:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-  except ImportError:
+  except Exception:
     pass
